d86ca16e99cbd97272dcbcdb5343f12c091e78a8
Kamari <klementoboh@yahoo.com>
2023-04-18 20:31:31 +0300

    Added rating progress

---
+++ src/baseUI/Rating-circle/index.jsx
@@ -0,0 +1,75 @@
+import { memo } from "react";
+
+const getColour = (rating) => {
+  if (rating >= 70) return "high";
+  if (rating >= 40) return "mid";
+  if (rating >= 0) return "low";
+  return "none";
+};
+
+const RatingCircle = ({ percent }) => {
+  const dashArray = Math.PI * 100;
+  const dashOffset = Math.PI * (100 - percent);
+
+  const bar = {
+    low: "#db2360",
+    mid: "#d2d531",
+    high: "#21d07a",
+    none: "#d4d4d4",
+  };
+  const track = {
+    low: "#571435",
+    mid: "#423d0f",
+    high: "#204529",
+    none: "#666666",
+  };
+
+  return (
+    <div className="w-[38px] h-[38px] bg-tmdbDarkBlue  rounded-full flex justify-center items-center">
+      <svg
+        width="34px"
+        height="34px"
+        viewBox="0 0 100 100"
+        className="rotate-[-90deg]"
+      >
+        {/* //track */}
+        <circle
+          cx="53"
+          cy="53"
+          r="50"
+          fill="transparent"
+          stroke={track[getColour(percent)]}
+          strokeWidth="6"
+          strokeDasharray={dashArray}
+          className="scale-[0.94]"
+        />
+
+        {/* //bar */}
+        <circle
+          cx="53"
+          cy="53"
+          r="50"
+          fill="transparent"
+          stroke={bar[getColour(percent)]}
+          strokeWidth="6"
+          strokeDasharray={dashArray}
+          strokeDashoffset={dashOffset}
+          strokeLinecap="round"
+          className="scale-[0.94]"
+        />
+      </svg>
+      <div className="font-semibold absolute text-white text-[.85rem]">
+        {percent ? (
+          <div>
+            {percent}
+            <sup className="text-[5px] align-super">%</sup>
+          </div>
+        ) : (
+          "NR"
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default memo(RatingCircle);
--- src/components/Movies/MovieCard.jsx
+++ src/components/Movies/MovieCard.jsx
@@ -1,3 +1,5 @@
+import RatingCircle from "../../baseUI/Rating-circle";
+
 const getPosterPath = (posterPath) =>
   `https://www.themoviedb.org/t/p/w220_and_h330_face/${posterPath}`;
 
@@ -7,15 +9,22 @@ const MovieCard = ({
   title,
   release_date,
   first_air_date,
+  vote_average,
 }) => {
   return (
-    <div className="flex flex-col">
-      <img
-        src={getPosterPath(poster_path)}
-        alt={name}
-        className="w-[150px] h-[225px] rounded-lg mb-6"
-      />
-      <div className="flex flex-col w-[150px]">
+    <div className="flex flex-col gap-2">
+      <div className="relative">
+        <img
+          src={getPosterPath(poster_path)}
+          alt={name}
+          className="w-[150px] h-[225px] shadow-sm rounded-lg"
+        />
+        {/* <div className="absolute top-3 right-[10px]">ellipsis</div> */}
+        <div className="absolute bottom-[-1.2rem] left-2">
+          <RatingCircle percent={vote_average * 10} />
+        </div>
+      </div>
+      <div className="flex flex-col pt-5 w-[150px]">
         <h3 className="font-bold">{name || title}</h3>
         <p className="font-normal text-slate-500">
           {first_air_date || release_date}
